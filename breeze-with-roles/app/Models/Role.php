1d6e9703c018ced4b040cba3c8b6aa04a604cc5e
webdevsunil <webdevelopersunil>
2023-11-02 23:20:51 +0530

    User Roles

--- breeze-with-roles/app/Models/Role.php
+++ breeze-with-roles/app/Models/Role.php
@@ -15,5 +15,5 @@ class Role extends Model
         "name",
         "status"
     ];
-    
+
 }
--- breeze-with-roles/app/Models/UserRole.php
+++ breeze-with-roles/app/Models/UserRole.php
@@ -16,5 +16,13 @@ class UserRole extends Model
         "role_id"
     ];
 
+    public function user() : BelongsTo
+    {
+        return $this->BelongsTo(User::class,'user_id','id');
+    }
 
+    public function role() : BelongsTo
+    {
+        return $this->BelongsTo(Role::class,'role_id','id');
+    }
 }
